f80c3a315b1a3277e2706597740e73480704d0e0
Mark Leybourne <mleyb@live.co.uk>
2017-07-10 19:00:33 +0100

    Initial

---
+++ PushoverClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PushoverNet
+{
+    public class PushoverClient
+    {
+        private readonly string _appKey;
+
+        public PushoverClient(string appKey)
+        {
+            _appKey = appKey;
+        }
+
+        public async Task SendAsync(string userKey, string message)
+        {
+            using (var client = new HttpClient())
+            {
+                // TODO
+                await client.PostAsync("", new StringContent(""));
+            }
+        }
+    }
+}
