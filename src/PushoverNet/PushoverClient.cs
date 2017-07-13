f19730403352bd5a4ea670dc10b69a162e86c398
Mark Leybourne <mleyb@users.noreply.github.com>
2017-07-13 17:16:31 +0100

    Update PushoverClient.cs

--- src/PushoverNet/PushoverClient.cs
+++ src/PushoverNet/PushoverClient.cs
@@ -1,9 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace PushoverNet
 {
